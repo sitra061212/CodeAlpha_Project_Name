6ed2c8eba2f846622a39670c6ce5220f85f1de29
sitra <sitra.f.khairo515@gmail.com>
2024-08-31 11:35:19 +0000

    route file

--- routes/url.js
+++ routes/url.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 import { generate } from 'shortid';
-import Url from '../models/Url.js'; // Import the Url model
+import Url from '../models/Url.js'; 
 
 const router = Router();
 
 // POST /api/url/shorten
 router.post('/shorten', async (req, res) => {
   const { longUrl } = req.body;
-  const baseUrl = 'http://localhost:5000';
+  const baseUrl = 'http://localhost:7000/api/url';
 
   const urlCode = generate();
   try {
-    let url = await Url.findOne({ longUrl }); // Use findOne method directly
+    let url = await Url.findOne({ longUrl }); 
     if (url) {
       res.json(url);
     } else {
@@ -32,10 +32,10 @@ router.post('/shorten', async (req, res) => {
 
 // GET /:urlCode
 router.get('/:urlCode', async (req, res) => {
-  console.log('URL Code:', req.params.urlCode); // Log the URL code
+  console.log('URL Code:', req.params.urlCode); 
   try {
-      const url = await Url.findOne({ urlCode: req.params.urlCode }); // Use the Url model to query
-      console.log('Short URL:', url); // Log the Url object
+      const url = await Url.findOne({ urlCode: req.params.urlCode }); 
+      console.log('Short URL:', url.shortUrl); 
       if (!url) {
           return res.status(404).json({ msg: 'Short URL not found' });
       }
