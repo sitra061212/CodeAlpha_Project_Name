4585672e5eb5fa32546ffba4ec5c668c40414dd4
sitra <sitra.f.khairo515@gmail.com>
2024-08-31 11:35:08 +0000

    database configuration

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await connect('mongodb://birna123:admin123@codealpha-shard-00-00.3b0ww.mongodb.net:27017,codealpha-shard-00-01.3b0ww.mongodb.net:27017,codealpha-shard-00-02.3b0ww.mongodb.net:27017/?replicaSet=atlas-bi2kdu-shard-0&ssl=true&authSource=admin');
+    await connect('mongodb://birna123:admin123@codealpha-shard-00-00.3b0ww.mongodb.net:27017,codealpha-shard-00-01.3b0ww.mongodb.net:27017,codealpha-shard-00-02.3b0ww.mongodb.net:27017/UrlShortner?replicaSet=atlas-bi2kdu-shard-0&ssl=true&authSource=admin');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
